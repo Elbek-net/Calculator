99f5eafbee1c13e4a4fdfff337725851245d6140
Elbek-net <xudoyberdiyevabdumalik134@gmail.com>
2025-03-19 12:16:28 +0500

    INTRO: Upload project

--- Program.cs
+++ Program.cs
@@ -3,5 +3,14 @@ Console.WriteLine("Birinchi raqamni kriting: ");
 int firstnumber=int.Parse(Console.ReadLine());
 Console.WriteLine("Ikkinchi raqamni kriting: ");
 int secondnumber=int.Parse(Console.ReadLine());
-Console.WriteLine($"Firstnumber is bigger than Secondnumber: {firstnumber>secondnumber}");
-Console.WriteLine($"Firstnumber is smaller than Secondnumber: {firstnumber<secondnumber}");
+
+if (firstnumber > secondnumber)
+{
+    Console.WriteLine("Firstnumber is bigger  than Secondnumber.");
+}
+else if(firstnumber == secondnumber)
+{
+     Console.WriteLine("They are equal."); 
+}
+else
+Console.WriteLine("Firstnumber is smaller than Secondnumber.");
